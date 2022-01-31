dbd8a9b05b901fadea4c5571a1c4b86b8822fd51
Kelvin Njiraini <njirainikelvin@gmail.com>
2022-01-31 09:57:28 +0300

    start of responsiveness

--- src/App.vue
+++ src/App.vue
@@ -24,6 +24,24 @@ html {
     font-size: 62.5%;
 }
 
+@media only screen and (max-width: 800px) {
+    html {
+        font-size: 50%;
+    }
+}
+
+@media only screen and (max-width: 600px) {
+    html {
+        font-size: 43.75%;
+    }
+}
+
+@media only screen and (max-width: 510px) {
+    html {
+        font-size: 37.5%;
+    }
+}
+
 body {
     background-color: #f5f5f5;
 }
