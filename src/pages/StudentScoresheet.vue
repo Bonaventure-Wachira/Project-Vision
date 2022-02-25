3399702f53129f59340d5d0941e3d4d3762d0dd5
Kelvin Njiraini <njirainikelvin@gmail.com>
2022-02-25 09:10:34 +0300

    results table component added

---
+++ src/pages/StudentScoresheet.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>My scoresheet</h1>
+
+        <div v-if="exams.length == 0">
+            <base-card>
+                <h2>You do not have any exam results here just yet.</h2>
+                <h4>Would you like to get started?</h4>
+            </base-card>
+        </div>
+        <div v-else>
+            <h2>Here are your records</h2>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            exams: [],
+        };
+    },
+};
+</script>
